af5c91aa568f3d0628ead5144f6a7e0d687fe458
janfb <j.f.boelts@gmail.com>
2017-03-23 09:16:06 +0100

    implement file reading for stimulation data set in matlab. write script for loading and plotting the raw data in python.

--- definitions.py
+++ definitions.py
@@ -4,6 +4,12 @@ ROOT_DIR = '/Users/Jan/Dropbox/Master/LR_Kuehn/'
 
 DATA_PATH = os.path.join(ROOT_DIR, 'data', 'dystonia_rest', 'for_python')
 
+DATA_PATH_BAROW = os.path.join(ROOT_DIR, 'data', 'dystonia_stim', 'for_python')
+
 SAVE_PATH_DATA = os.path.join(ROOT_DIR, 'analysis_data')
 
+SAVE_PATH_DATA_BAROW = os.path.join(ROOT_DIR, 'analysis_data', 'barow_data')
+
 SAVE_PATH_FIGURES = os.path.join(ROOT_DIR, 'analysis_figures')
+
+SAVE_PATH_FIGURES_BAROW = os.path.join(ROOT_DIR, 'analysis_figures', 'barow_data')
---
+++ python/barow_data/read_mat_save_and_plot.py
@@ -0,0 +1,65 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from definitions import SAVE_PATH_FIGURES_BAROW, DATA_PATH_BAROW, SAVE_PATH_DATA_BAROW
+import python.utils as ut
+
+
+file_list = os.listdir(DATA_PATH_BAROW)
+save_folder = os.path.join(SAVE_PATH_DATA_BAROW, 'raw')
+
+n_subjects = 16
+suffix = ''
+
+for sub in np.arange(1, n_subjects + 1):
+
+    # save all data of current subject in dict
+    subject_dict = dict(filenames={}, lfp={}, fs={}, conditions={}, chanlabels={}, time={}, id={})
+    plt.figure(figsize=(7, 5))
+    plot_idx = 1
+    for f in file_list:
+        # process only the current subject
+        if f.startswith('spmeeg_{}_'.format(sub)) and not f.startswith('spmeeg_{}_d'.format(sub)):
+            current_file_name = f[:-4]
+            print('plotting from {}'.format(f))
+            d = ut.load_data_spm(f, data_folder=DATA_PATH_BAROW)
+            fs = d['fsample'][0][0]
+            chanlabels = d['chanlabels'][0][0]
+            condition = d['condition_str'][0]
+
+            # save data
+            subject_dict['lfp'][condition] = d['data'][0, :]
+            subject_dict['filenames'][condition] = current_file_name
+            subject_dict['fs'][condition] = fs
+            subject_dict['conditions'][condition] = condition
+            subject_dict['id'][condition] = d['subject_id'][0]
+            subject_dict['chanlabels'][condition] = chanlabels[0]
+            subject_dict['number'] = sub
+
+            # downsample to make plotting faster
+            down_factor = 300
+            time = d['time'][0, ::down_factor]
+            lfp = d['data'][0, ::down_factor]
+
+            plt.subplot(3, 1, plot_idx)
+            plt.plot(time, lfp)
+            plt.ylabel('lfp [mu V]')
+            plt.title('Channel {} in condition {}'.format(chanlabels[0], condition))
+            if plot_idx < 3:
+                plt.xticks([], [])
+            else:
+                plt.xlabel('Time [s]')
+            plot_idx += 1
+            plt.suptitle('Subject file {}, id {}'.format(current_file_name, d['subject_id'][0]))
+
+    # save data
+    ut.save_data(data_dict=subject_dict,
+                 filename='subject_{}_allconditions_raw.p'.format(sub),
+                 folder=save_folder)
+    plt.savefig(os.path.join(SAVE_PATH_FIGURES_BAROW, 'raw', current_file_name + '_raw.pdf'.format(suffix)))
+    # plt.show()
+    plt.close()
+    # break
+
+
+
