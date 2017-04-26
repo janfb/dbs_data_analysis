Fix the bug that caused the unaligned psds. The frequency location of the peak was not saved correctly in the psd calculation.

resting_data_analysis/align_psds.py
@@ -14,7 +14,7 @@ For the theta and the beta range separately align the peaks and plot for. plot t
 
 
 # load data
-suffix = '_linear_search'
+suffix = '_linear_search_and_amp'
 data = ut.load_data_analysis('psd_maxamp_theta_beta{}.p'.format(suffix))
 # now make a plot of all selected channels and the histogram over peaks
 frequs = data['frequs']
@@ -23,7 +23,7 @@ print(data.keys())
 n_subjects, n_psd_samples = data['psd_beta'].shape
 
 # exclude subjects with too small peak amps in theta
-threshold = 4
+threshold = 2
 subject_mask = data['theta_peaks_max'] > threshold
 theta_peaks = data['theta_peaks_max'][subject_mask]
 theta_psd = data['psd_theta'][subject_mask]
@@ -34,51 +34,38 @@ theta_psd = data['psd_theta'][subject_mask]
 mean_peak_theta = data['theta_peaks_all'].mean()
 mean_peak_beta = data['beta_peaks_all'].mean()
 
-psd_length_theta = np.sum(ut.get_array_mask(frequs > mean_peak_theta - 3, frequs < mean_peak_theta + 8))
-psd_length_beta = np.sum(ut.get_array_mask(frequs > mean_peak_beta - 3, frequs < mean_peak_beta + 8))
+frequ_samples = 12
 
-psd_range_mat_theta = np.zeros((subject_mask.sum(), psd_length_theta))
-psd_range_mat_beta = np.zeros((n_subjects, psd_length_beta))
+psd_range_mat_theta = -1 * np.ones((subject_mask.sum(), frequ_samples))
+psd_range_mat_beta = -1 * np.ones((n_subjects, frequ_samples))
 
 for sub in range(n_subjects):
     # get the peak
     peak_beta = data['beta_peaks_max'][sub]
 
     # build a mask -3, +8 Hz around the peak
-    mask_beta = ut.get_array_mask(frequs > peak_beta - 3, frequs < peak_beta + 8)
+    mask_beta = ut.get_array_mask(frequs > (peak_beta - 3), frequs < (peak_beta + 8))
 
-    # if we are unlucky and the range holds one additional bin
-    # same for beta
-    if mask_beta.sum() > psd_length_beta:
-        # set the first bin in the range to false
-        first_idx = np.where(mask_beta)[0][0]
-        mask_beta[first_idx] = False
+    beta_frequs = frequs[mask_beta]
 
     # collect the psd of this mask in a matrix
     psd_range_mat_beta[sub, ] = data['psd_beta'][sub, mask_beta]
 
-for sub in range(subject_mask.sum()):
+for sub in range(np.sum(subject_mask)):
     # get the peak
     peak_theta = theta_peaks[sub]
 
     # build a mask -3, +8 Hz around the peak
     mask_theta = ut.get_array_mask(frequs > peak_theta - 3, frequs < peak_theta + 8)
 
-    # if we are unlucky and the range holds one additional bin
-    if mask_theta.sum() > psd_length_theta:
-        # set the first bin in the range to false
-        first_idx = np.where(mask_theta)[0][0]
-        mask_theta[first_idx] = False
-
     # collect the psd of this mask in a matrix
     psd_range_mat_theta[sub, ] = data['psd_theta'][sub, mask_theta]
 
-mask = ut.get_array_mask(frequs > 2, frequs < 13)
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
-freq_range = np.linspace(-3, 8, psd_length_theta)
+freq_range = np.linspace(-3, 8, frequ_samples)
 # comment in to plot individual psds
-# plt.plot(freq_range, psd_range_mat_theta.T, alpha=.2, color='C1')
+plt.plot(freq_range, psd_range_mat_theta.T, alpha=.2, color='C1')
 
 # plot standard error around mean
 mean = psd_range_mat_theta.mean(axis=0)
@@ -94,9 +81,9 @@ plt.ylim([0, 7])
 plt.legend()
 
 plt.subplot(1, 2, 2)
-freq_range = np.linspace(-3, 8, psd_length_beta)
+freq_range = np.linspace(-3, 8, frequ_samples)
 # comment in to plot individual psds
-# plt.plot(freq_range, psd_range_mat_beta.T, alpha=.2, color='C1')
+plt.plot(freq_range, psd_range_mat_beta.T, alpha=.2, color='C1')
 
 # plot standard error around mean
 mean = psd_range_mat_beta.mean(axis=0)
@@ -111,5 +98,6 @@ plt.title('Beta peaks aligned')
 plt.ylim([0, 7])
 plt.legend()
 
-plt.savefig(os.path.join(SAVE_PATH_FIGURES, 'figure_3AB_{}Hz_cutoff.pdf'.format(threshold)))
+plt.savefig(os.path.join(SAVE_PATH_FIGURES, 'figure_3AB_{}Hz_cutoff_all.pdf'.format(threshold)))
 # plt.show()
+plt.close()

resting_data_analysis/calculate_and_plot_psd.py
@@ -12,16 +12,16 @@ n_frequ_samples = int(window_length / 2 + 1)
 n_patients = 27
 n_electrodes = 153
 max_amp_psds_theta = np.zeros((n_patients, n_frequ_samples))
-max_amp_peak_theta = np.zeros(n_patients)
+max_amp_peak_frequency_theta = np.zeros(n_patients)
 max_amp_psds_beta = np.zeros((n_patients, n_frequ_samples))
-max_amp_peak_beta = np.zeros(n_patients)
+max_amp_peak_frequency_beta = np.zeros(n_patients)
 
 theta_peaks = np.zeros(n_electrodes)
 beta_peaks = np.zeros(n_electrodes)  # there are supposed to be 153 channels
 frequs = None
 # subject counters
-i_subjects = 0
-i_channels = 0
+subject_idx = 0
+electrode_idx = 0
 
 for f in file_list:
     if f.startswith('spmeeg_'):
@@ -30,15 +30,18 @@ for f in file_list:
         chanlabels = d['chanlabels'][0]
 
         # prelocate amp arrays for saving the peak amp of every channel
-        beta_amp = np.zeros(d['data'].shape[0])
-        theta_amp = np.zeros(d['data'].shape[0])
-        psds = np.zeros((d['data'].shape[0], n_frequ_samples))
+        n_channels = d['data'].shape[0]
+        beta_amp = np.zeros(n_channels)
+        theta_amp = np.zeros(n_channels)
+        psds = np.zeros((n_channels, n_frequ_samples))
+        theta_peak_frequency = np.zeros(n_channels)
+        beta_peak_frequency = np.zeros(n_channels)
 
         plt.figure(figsize=(10, 5))
-        for i, lfp in enumerate(d['data']):
+        for channel_idx, lfp in enumerate(d['data']):
             # remove 50Hz noise
-            # lfp_clean = lfp # band_pass_filter(lfp, band=2, fs=fs, btype='highpass')
-            lfp_clean = ut.band_stop_filter(lfp, fs, band=[49, 51], plot_response=False)
+            lfp_clean = lfp
+            # lfp_clean = ut.band_stop_filter(lfp, fs, band=[49, 51], plot_response=False)
             # calculate psd
             frequs, psd = ut.calculate_psd(lfp_clean, fs=fs, window_length=window_length)
 
@@ -50,59 +53,65 @@ for f in file_list:
             # frequs_linear, psd_linear = ut.remove_1f_component(frequs, psd)
 
             # save for later
-            psds[i, ] = psd
+            psds[channel_idx,] = psd
             # find peak in theta: use the linearized spectra for finding peaks
             idx_theta, peak_amp_theta, ftheta, psd_theta = ut.find_peak_in_band(frequs, psd, [4, 12], linearize=True)
-            # take the mean over peak +- 1.5Hz
-            # surround_mask = ut.get_array_mask(frequs > (ftheta[idx_theta] - 1.5), frequs < (ftheta[idx_theta] + 1.5))
-            theta_amp[i] = peak_amp_theta  # np.mean(psd[surround_mask])
-            theta_peaks[i_channels] = ftheta[idx_theta]
+            # save the frequency at which the peak occured, in one long list for the histogram
+            theta_peaks[electrode_idx] = ftheta[idx_theta]
+            # save it in a matrix to select it per subject for the alignment
+            theta_peak_frequency[channel_idx] = ftheta[idx_theta]
+            # save the amplitude of the normalized psd
             theta_peak = psd_theta[idx_theta]
+            # save the amplitude of the theta peak from the search algorithm
+            theta_amp[channel_idx] = peak_amp_theta  # np.mean(psd[surround_mask])
 
             # find peak in beta
             idx_beta, peak_amp_beta, fbeta, psd_beta = ut.find_peak_in_band(frequs, psd, [12, 30], linearize=True)
             surround_mask = ut.get_array_mask(frequs > (fbeta[idx_beta] - 1.5), frequs < (fbeta[idx_beta] + 1.5))
             beta_peak = psd_beta[idx_beta]
-            beta_amp[i] = peak_amp_beta  # np.mean(psd[surround_mask])
-            beta_peaks[i_channels] = fbeta[idx_beta]
+            beta_amp[channel_idx] = peak_amp_beta  # np.mean(psd[surround_mask])
+            beta_peak_frequency[channel_idx] = fbeta[idx_beta]
+            beta_peaks[electrode_idx] = fbeta[idx_beta]
 
             # plotting
             mask = ut.get_array_mask(frequs > 4, frequs < 30)
-            plt.subplot(3, 2, i + 1)
+            plt.subplot(3, 2, channel_idx + 1)
             plt.plot(frequs[mask], psd[mask], label='psd')
             plt.plot(ftheta[idx_theta], theta_peak, 'o', label='theta peak')
             plt.plot(fbeta[idx_beta], beta_peak, 'o', label='beta peak')
             plt.legend()
-            plt.title('LFP channel {}'.format(chanlabels[i]))
-            if not(i == 4 or i == 5):
+            plt.title('LFP channel {}'.format(chanlabels[channel_idx]))
+            if not(channel_idx == 4 or channel_idx == 5):
                 plt.xticks([], [])
 
             # count channels in total
-            i_channels += 1
+            electrode_idx += 1
 
         # now select the channel with highest amplitude
         beta_channel = np.argmax(beta_amp)
         theta_channel = np.argmax(theta_amp)
-        max_amp_psds_theta[i_subjects, ] = psds[theta_channel]
-        max_amp_peak_theta[i_subjects] = ftheta[theta_channel]
+        max_amp_psds_theta[subject_idx,] = psds[theta_channel,]
+        max_amp_peak_frequency_theta[subject_idx] = theta_peak_frequency[theta_channel]
 
-        max_amp_psds_beta[i_subjects, ] = psds[beta_channel]
-        max_amp_peak_beta[i_subjects] = fbeta[beta_channel]
+        max_amp_psds_beta[subject_idx,] = psds[beta_channel]
+        max_amp_peak_frequency_beta[subject_idx] = beta_peak_frequency[beta_channel]
 
         # note which channel was selected
         plt.subplot(3, 2, theta_channel + 1)
-        plt.title('LFP channel {}, selected theta'.format(chanlabels[i]))
+        plt.axvline(max_amp_peak_frequency_theta[subject_idx])
+        plt.title('LFP channel {}, selected theta'.format(chanlabels[channel_idx]))
         plt.subplot(3, 2, beta_channel + 1)
-        plt.title('LFP channel {}, selected beta'.format(chanlabels[i]))
+        plt.axvline(max_amp_peak_frequency_beta[subject_idx])
+        plt.title('LFP channel {}, selected beta'.format(chanlabels[channel_idx]))
 
         # save figure
         plt.savefig(os.path.join(SAVE_PATH_FIGURES, f[:-4] + '_psd{}.pdf'.format(suffix)))
         # plt.show()
         plt.close()
-        i_subjects += 1
+        subject_idx += 1
 
 save_dict = dict(filelist=file_list, psd_theta=max_amp_psds_theta, psd_beta=max_amp_psds_beta,
-                 theta_peaks_all=theta_peaks, theta_peaks_max=max_amp_peak_theta,
-                 beta_peaks_all=beta_peaks, beta_peaks_max=max_amp_peak_beta,
-                 frequs=frequs, n_subjects=i_subjects, n_channels=i_channels)
+                 theta_peaks_all=theta_peaks, theta_peaks_max=max_amp_peak_frequency_theta,
+                 beta_peaks_all=beta_peaks, beta_peaks_max=max_amp_peak_frequency_beta,
+                 frequs=frequs, n_subjects=subject_idx, n_channels=electrode_idx)
 ut.save_data(save_dict, 'psd_maxamp_theta_beta{}.p'.format(suffix))