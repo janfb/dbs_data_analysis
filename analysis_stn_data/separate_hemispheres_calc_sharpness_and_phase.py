48844ef462a97fc156f7fbf3e0e8444af8b95075
janfb <j.f.boelts@gmail.com>
2017-06-10 13:23:08 +0200

    add extraction of beta amplitudes.

--- analysis_stn_data/separate_hemispheres_calc_sharpness_and_phase.py
+++ analysis_stn_data/separate_hemispheres_calc_sharpness_and_phase.py
@@ -18,7 +18,7 @@ data_folder = os.path.join(DATA_PATH, 'STN_data_PAC', 'collected')
 file_list = os.listdir(data_folder)
 save_folder = os.path.join(SAVE_PATH_DATA, 'stn')
 
-subject_file_list = [file for file in file_list if file.startswith('subject_MC') and file.endswith('.p')]
+subject_file_list = [file for file in file_list if file.startswith('subject') and file.endswith('.p')]
 
 data_dict = dict()
 
@@ -53,6 +53,7 @@ for file_idx, file in enumerate(subject_file_list):
     esr_mat = np.zeros((n_channels, n_bands, n_conditions, 2))  # last dim is for mean, std
     rdsr_mat = np.zeros((n_channels, n_bands, n_conditions, 2))
     meanPhaseVec_mat = np.zeros((n_channels, n_bands, n_conditions, 2))  # last to dims for amplitude and angle
+    beta_amp_mat = np.zeros((n_channels, n_bands, n_conditions))
 
     for condition_idx, condition in enumerate(conditions):
         # get the data dict of the current condition
@@ -61,9 +62,9 @@ for file_idx, file in enumerate(subject_file_list):
         n_samples, n_epochs = condition_dict['data'].shape[1:]
 
         # select channels per hemisphere to treat them as separate subjects
-        channel_labels = np.squeeze(condition_dict['channels'])
+        channel_labels_local = np.squeeze(condition_dict['channels'])
 
-        for channel_idx, channel_label in enumerate(channel_labels):
+        for channel_idx, channel_label in enumerate(channel_labels_local):
 
             channel_label = channel_label[0]
             channel_lfp = condition_dict['data'][channel_idx]
@@ -80,6 +81,7 @@ for file_idx, file in enumerate(subject_file_list):
                 rdsr = np.zeros(n_epochs)
                 mpl = np.zeros(n_epochs)  # mean phase vector length
                 mpa = np.zeros(n_epochs)  # amplitude
+                beta_amp = np.zeros(n_epochs)
 
                 # for every epoch
                 for epoch_idx, epoch in enumerate(channel_lfp.T):
@@ -87,6 +89,14 @@ for file_idx, file in enumerate(subject_file_list):
                     # do preprocessing a la Cole et al
                     # low pass filter
                     lfp_pre = ut.low_pass_filter(y=epoch, fs=fs, cutoff=200)
+
+                    # extract beta amplitude
+                    # calculate psd
+                    frequs, psd = ut.calculate_psd(lfp_pre, fs=fs, window_length=1024)
+                    # get the mask of the current band
+                    band_mask = ut.get_array_mask(frequs >= current_band[0], frequs <= current_band[1])
+                    # calculate beta amplitude
+                    beta_amp[epoch_idx] = np.mean(psd[band_mask])
 
                     # band pass filter
                     lfp_band = ut.band_pass_iir(y=lfp_pre, fs=fs, band=current_band)
@@ -110,13 +120,16 @@ for file_idx, file in enumerate(subject_file_list):
                 esr_mat[channel_idx, band_idx, condition_idx, :] = esr.mean(), esr.std()
                 rdsr_mat[channel_idx, band_idx, condition_idx, :] = rdsr.mean(), rdsr.std()
                 meanPhaseVec_mat[channel_idx, band_idx, condition_idx, :] = mpl.mean(), mpa.mean()
+                beta_amp_mat[channel_idx, band_idx, condition_idx] = np.mean(beta_amp)
 
     # now we have a matrix with channels x bands x conditions x (mean, std) for one subject
 
     # now extract the PAC data
     # the frequency resolution should be the same for conditions
-    n_amplitude = pac_dict['on']['F_amp'].size
-    n_phase = pac_dict['on']['F_phase'].size
+    f_phase = pac_dict['on']['F_phase']
+    f_amp = pac_dict['on']['F_amp']
+    n_amplitude = f_amp.size
+    n_phase = f_phase.size
 
     # prelocat the pac matrix over all channels because we have to iterate over conditions first
     pac_matrix = np.zeros((len(channel_labels), n_conditions, n_amplitude, n_phase))
@@ -129,7 +142,7 @@ for file_idx, file in enumerate(subject_file_list):
         # extract data only for the current hemisphere channels
         for channel_idx, channel_string in enumerate(channel_labels):
 
-            channel_key = 'PAC_{}'.format(channel_string[0])
+            channel_key = 'PAC_{}'.format(channel_string)
             pac_matrix[channel_idx, condition_idx, ] = condition_dict[channel_key]
 
     # we now separate the hemispheres into separate subjects
@@ -142,11 +155,12 @@ for file_idx, file in enumerate(subject_file_list):
                                rdsr_mat=rdsr_mat[left_channel_idx, ],
                                pac_matrix=pac_matrix[left_channel_idx, ],
                                meanPhaseVec_mat=meanPhaseVec_mat[left_channel_idx, ],
+                               beta_amp_mat=beta_amp_mat[left_channel_idx, ],
                                f_amp=pac_dict['on']['F_amp'],
                                f_phase=pac_dict['on']['F_phase'],
                                id=key_left,
                                conditions=conditions,
-                               channel_labels=channel_labels[left_channel_idx],
+                               channel_labels=np.array(channel_labels)[left_channel_idx],
                                condition_bands=super_dict['bands']['left'],
                                significance_flag=super_dict['sig_flag_matrix'][left_channel_idx, ])
 
@@ -154,12 +168,13 @@ for file_idx, file in enumerate(subject_file_list):
     data_dict[key_right] = dict(esr_mat=esr_mat[right_channel_idx,],
                                 rdsr_mat=rdsr_mat[right_channel_idx,],
                                 pac_matrix=pac_matrix[right_channel_idx, ],
-                                meanPhaseVec_mat=meanPhaseVec_mat[right_channel_idx,],
+                                meanPhaseVec_mat=meanPhaseVec_mat[right_channel_idx, ],
+                                beta_amp_mat=beta_amp_mat[right_channel_idx, ],
                                 f_amp=pac_dict['on']['F_amp'],
                                 f_phase=pac_dict['on']['F_phase'],
                                 id=key_right,
                                 conditions=conditions,
-                                channel_labels=channel_labels[right_channel_idx],
+                                channel_labels=np.array(channel_labels)[right_channel_idx],
                                 condition_bands=super_dict['bands']['right'],
                                 significance_flag=super_dict['sig_flag_matrix'][right_channel_idx, ])
 
