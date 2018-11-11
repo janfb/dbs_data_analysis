d8af16697fafe9dafea41cdf0a5ed5f7453f70d6
janfb <j.f.boelts@gmail.com>
2018-11-11 19:40:31 +0100

    remove key betabands and add it to values of last dict level.

--- analysis_stn_data/calculate_measures_per_subject.py
+++ analysis_stn_data/calculate_measures_per_subject.py
@@ -123,12 +123,12 @@ for subject_idx, subject_id in enumerate(subject_list):
     if condition_list[subject_idx] not in result_dict[subject_id][subject_channels[channel_idx]].keys():
         result_dict[subject_id][subject_channels[channel_idx]][condition_list[subject_idx]] = dict()
 
-    if str(beta_bands[subject_idx]) not in result_dict[subject_id][subject_channels[channel_idx]][condition_list[subject_idx]].keys():
-        result_dict[subject_id][subject_channels[channel_idx]][condition_list[subject_idx]][str(beta_bands[subject_idx])] = dict()
+    # if str(beta_bands[subject_idx]) not in result_dict[subject_id][subject_channels[channel_idx]][condition_list[subject_idx]].keys():
+    #     result_dict[subject_id][subject_channels[channel_idx]][condition_list[subject_idx]][str(beta_bands[subject_idx])] = dict()
 
     # save values for the current subject
-    result_dict[subject_id][subject_channels[channel_idx]][condition_list[subject_idx]][str(beta_bands[subject_idx])] =\
-        dict(esr=esr_mat, rdsr=rdsr_mat, pvl=meanPhaseVec_mat)
+    result_dict[subject_id][subject_channels[channel_idx]][condition_list[subject_idx]] =\
+        dict(betaband=beta_bands[subject_idx], esr=esr_mat, rdsr=rdsr_mat, pvl=meanPhaseVec_mat)
 
     print(result_dict[subject_id])
     # print results for testing
